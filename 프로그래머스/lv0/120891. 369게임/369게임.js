c83a35e96830bd946737a7fa516060345c6f3afa
se0jinYoon <109661444+se0jinYoon@users.noreply.github.com>
2023-04-30 20:52:19 +0900

    [level 0] Title: 369게임, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,7 +1,12 @@
-function solution(order) {
-    let answer = 0;
-    let orderS = order.toString();
-    [...orderS].forEach(v => {if (v == 3 || v ==6 || v== 9) answer +=1})
-    return answer;
+// function solution(order) {
+//     let answer = 0;
+//     let orderS = order.toString();
+//     [...orderS].forEach(v => {if (v == 3 || v ==6 || v== 9) answer +=1})
+//     return answer;
     
+// }
+
+function solution(order) {
+    var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+    return answer;
 }
