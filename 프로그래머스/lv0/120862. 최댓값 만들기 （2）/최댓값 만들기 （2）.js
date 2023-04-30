3bbf56a17e98df5832f64076b7b6ab808e327f8e
se0jinYoon <109661444+se0jinYoon@users.noreply.github.com>
2023-04-30 20:03:53 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,8 @@
 function solution(numbers) {
-    var answer = 0;
-    return answer;
+    numbers.sort((a,b) => b-a);
+    let max1 = numbers[0] * numbers[1];
+    let max2 = numbers.at(-1) * numbers.at(-2);
+    
+    return Math.max(max1, max2);
+    
 }
