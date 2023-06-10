f3659d3f0d26d8a4edee77b158adc3acb6f754ad
se0jinYoon <109661444+se0jinYoon@users.noreply.github.com>
2023-06-10 19:37:01 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,13 +1,9 @@
 function solution(n) {
     let three = "";
-    let answer = 0;
-    while (n > 0){
+    while (n != 0) {
+        n = Math.ceil(n / 3);
+        console.log(n);
         three += n % 3;
-        n = parseInt(n / 3);
     }
-    let reversed = [...three].reverse().join('');
-    for (let i = reversed.length-1; i >= 0; i--) {
-        answer += Number(reversed[i]) * (Math.pow(3,i));
-    }
-    return answer;
+    console.log(three);
 }
