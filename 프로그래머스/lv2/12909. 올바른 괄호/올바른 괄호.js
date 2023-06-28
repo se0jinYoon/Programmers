7714c473728976c74d05bcae14285fc2b28bd01d
se0jinYoon <109661444+se0jinYoon@users.noreply.github.com>
2023-06-28 20:56:44 +0900

    [level 2] Title: 올바른 괄호, Time: 2.25 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,17 @@
+function solution(s){
+    let stackCount = 0;
+    for (let i = 0 ;i<s.length; i++) {
+        stackCount += s[i] === "(" ? 1 : -1;
+        if (stackCount < 0) return false;
+    }
+    return stackCount === 0? true : false;
+}
+
+// function solution(s){
+//     let stackCount = 0;
+//     [...s].forEach(v => {
+//         stackCount += v === "(" ? 1 : -1;
+//         if (stackCount < 0) return false;
+//     })
+//     return stackCount === 0? true : false;
+// }
